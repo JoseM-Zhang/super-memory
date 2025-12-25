81161e5b0e7172071d2997f01807a9a3fc17e1f9
Zhang <josemzhangpan@gmail.com>
2025-12-24 22:02:45 -0500

    Update generateModelsJson.py

--- py/generateModelsJson.py
+++ py/generateModelsJson.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-parentDir = os.listdir("assets/spine/")
+parentDir = os.listdir("./assets/spine/")
 
 data = {}
 
@@ -9,21 +9,21 @@ for i in parentDir:
     # There may be more than one skel in the folder, and there's also possibility of pack mistakes.
     # Currently, not handling this.
     print(i)
-    file = [x for x in (os.listdir(f"assets/spine/{i}")) if ".skel" in x]
+    file = [x for x in (os.listdir(f"./assets/spine/{i}")) if ".skel" in x]
     if len(file) > 1:
         for j in file:
             if j[0] == "_":
                 continue
-            data[j[:-5]] = f"assets/spine/{i}/{j}"
+            data[j[:-5]] = f"./assets/spine/{i}/{j}"
     else:
         if i[0] == "_":
             continue
-        data[i] = f"assets/spine/{i}/" + ''.join(file)
+        data[i] = f"./assets/spine/{i}/" + ''.join(file)
 
 """
 for i in parentDir:
     print(i)
-    file = [x for x in (os.listdir(f"assets/spine/{i}")) if ".skel" in x]
+    file = [x for x in (os.listdir(f"./assets/spine/{i}")) if ".skel" in x]
     if len(file) > 1:
         # There should be only one skel in the folder
         # This happens when the developer packs by mistake
@@ -33,7 +33,7 @@ for i in parentDir:
             if j[:-5].lower() not in i.lower():
                 # skel name should be the same as the folder name
                 continue
-            data[j[:-5]] = f"assets/spine/{i}/{j}"
+            data[j[:-5]] = f"./assets/spine/{i}/{j}"
     elif len(file) == 0:
         # No skel file in the folder
         continue
@@ -41,7 +41,7 @@ for i in parentDir:
         # Only one skel file in the folder
         if i[0] == "_":
             continue
-        data[i] = f"assets/spine/{i}/" + ''.join(file)
+        data[i] = f"./assets/spine/{i}/" + ''.join(file)
 """
 
 
